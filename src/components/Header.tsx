073cd3e3ff993adba40abf38314b90645903d6b7
hanjsnote <enlun@naver.com>
2025-11-27 10:40:10 +0900

    feat:컴포넌트 분리, Header.tsx

---
+++ src/components/Header.tsx
@@ -0,0 +1,64 @@
+type HeaderProps = {
+  isLoggedIn: boolean
+  onToggleMenu: () => void
+  isMenuOpen: boolean
+  onClickLogin: () => void
+  onClickLogout: () => void
+  onClearMessages: () => void
+}
+
+export function Header({
+  isLoggedIn,
+  isMenuOpen,
+  onToggleMenu,
+  onClickLogin,
+  onClickLogout,
+  onClearMessages,
+}: HeaderProps) {
+  return (
+    <header className="app-header">
+      <div className="menu-wrapper">
+        <button
+          type="button"
+          className="menu-button"
+          onClick={onToggleMenu}
+        >
+          ···
+        </button>
+
+        {isMenuOpen && (
+          <div className="menu-dropdown">
+            {!isLoggedIn && (
+              <button
+                type="button"
+                className="menu-item"
+                onClick={onClickLogin}
+              >
+                로그인
+              </button>
+            )}
+
+            {isLoggedIn && (
+              <>
+                <button
+                  type="button"
+                  className="menu-item"
+                  onClick={onClickLogout}
+                >
+                  로그아웃
+                </button>
+                <button
+                  type="button"
+                  className="menu-item"
+                  onClick={onClearMessages}
+                >
+                  대화 내역 삭제
+                </button>
+              </>
+            )}
+          </div>
+        )}
+      </div>
+    </header>
+  )
+}
